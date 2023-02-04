124767f5bc519fbdc873d5b9826c027166373cfb
jjohnson673 <jkjohnson673@gmail.com>
2023-02-04 13:57:42 -0500

    had some quoting inconsistencies

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -4,7 +4,7 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
-  butInstall.classList.toggle("hidden", false);
+  butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
@@ -15,7 +15,7 @@ butInstall.addEventListener('click', async () => {
   }
   promptEvent.prompt();
   window.deferredPrompt = null;
-  butInstall.classList.toggle("hidden", true);
+  butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
